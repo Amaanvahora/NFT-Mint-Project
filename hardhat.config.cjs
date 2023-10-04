71b1079781e9c69dbf993b4861bb5cfe13686a82
Amaan Vahora <52988749+Amaanvahora@users.noreply.github.com>
2023-10-03 23:43:04 -0400

    Add files via upload

---
+++ hardhat.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+};
---
+++ payToMint.sol
@@ -0,0 +1,50 @@
+function NFTImage({ tokenId, getCount }) {
+  const contentId = 'PINATA_CONTENT_ID';
+  const metadataURI = `${contentId}/${tokenId}.json`;
+  const imageURI = `https://gateway.pinata.cloud/ipfs/${contentId}/${tokenId}.png`;
+
+  const [isMinted, setIsMinted] = useState(false);
+  useEffect(() => {
+    getMintedStatus();
+  }, [isMinted]);
+
+  const getMintedStatus = async () => {
+    const result = await contract.isContentOwned(metadataURI);
+    console.log(result)
+    setIsMinted(result);
+  };
+
+  const mintToken = async () => {
+    const connection = contract.connect(signer);
+    const addr = connection.address;
+    const result = await contract.payToMint(addr, metadataURI, {
+      value: ethers.utils.parseEther('0.05'),
+    });
+
+    await result.wait();
+    getMintedStatus();
+    getCount();
+  };
+
+  async function getURI() {
+    const uri = await contract.tokenURI(tokenId);
+    alert(uri);
+  }
+  return (
+    <div>
+      <img src={isMinted ? imageURI : 'img/placeholder.png'}></img>
+        <h5>ID #{tokenId}</h5>
+        {!isMinted ? (
+          <button onClick={mintToken}>
+            Mint
+          </button>
+        ) : (
+          <button onClick={getURI}>
+            Taken! Show URI
+          </button>
+        )}
+    </div>
+  );
+}
+
+export default Home;
